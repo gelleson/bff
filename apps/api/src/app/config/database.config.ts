5c31786e282bab2271dbb3c02ae35c7a5c620e1e
gelleson <go.gelleson@gmail.com>
2020-12-06 22:20:30 +0600

    fix: database url

--- apps/api/src/app/config/database.config.ts
+++ apps/api/src/app/config/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  uri: process.env.DATABASE || 'localhost',
+  uri: process.env.DATABASE_URL || 'localhost',
   logging: process.env.DATABASE_LOGGING || 'all',
   synchronize: process.env.DATABASE_SYNC || true,
 }));
