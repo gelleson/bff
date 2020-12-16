81bffdb76d38b7d1a31f6b1227828a11a99f08a7
gelleson <go.gelleson@gmail.com>
2020-12-17 00:46:56 +0600

    feat(finance-mobile): add finance mobile on ionic

---
+++ xplat/web/core/core.module.spec.ts
@@ -0,0 +1,15 @@
+describe('Core module generic test', () => {
+  it('Should be true', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+/*
+import { BffCoreModule } from './core.module';
+
+describe('BffCoreModule', () => {
+  it('should work', () => {
+    expect(new BffCoreModule(null)).toBeDefined();
+  });
+});
+*/
