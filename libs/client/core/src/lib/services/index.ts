0e4465a6a9ed2eec5ec0be7805d95186a7927de2
gelleson <go.gelleson@gmail.com>
2020-12-20 03:59:24 +0600

    fix(client-core): add category service to index file

--- libs/client/core/src/lib/services/index.ts
+++ libs/client/core/src/lib/services/index.ts
@@ -1,5 +1,6 @@
 export * from './finance/account.service';
 export * from './event.service';
 export * from './finance/transaction.service';
-export * from './finance/subscription.service';
+export * from './finance/transaction.service';
+export * from './finance/category.service';
 export * from './auth';
